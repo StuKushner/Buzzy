ccf8ab51f978b62fcac934848427877b9c568933
Stuart Kushner <stuartkushner@Stuarts-MacBook-Pro.local>
2017-12-06 19:43:21 -0500

    more stuff

--- assets/app.js
+++ assets/app.js
@@ -36,49 +36,62 @@ function runQuery(numRecipes, queryURL) {
 
 			//Confirm that the JSON isn't missing any details
 			//If the recipe has a title, include it with the drink picture
+			if (alcoholData.response.drinks[i].strIngredient1 !== "null") {
+				$("#recipe-well-" + recipeCounter).append(
+					"<h3 class='nameOfCocktail><span class='label label-primary'>" +
+					recipeCounter + "<span><strong> " +
+					alcoholData.response./*whatever*/.drinkID + "</strong></h3>"
+					);
+			}
 
 			//Log name of cocktail to console
+			console.log(nameOfCocktail /*Placeholder*/)	
 
-			//if the recipe has the ingredients typed in, put that ingredient
-
-			//log the ingredients
-
-			//Put the rest of the ingredients in the html
-
-			//log them to console
+			//get drink id of first 5
 
 		}
 
 	});
 }
-
+//When user presses submit button
 $("#add-submit-button").click(function(event){
+	//Hitting "Enter" on keyboard will work
 	event.preventDefault();
+	//Initially sets recipeCounter to 0
 	recipeCounter = 0;
+	//Empties all of the wells
 	$("#well-section").empty();
+	//The user puts in the first ingredient and updates the URL with it
 	ingre1 = $("#first-ingredient").val().trim();
 	var searchURL = queryURLbase + ingre1;
-
+	//Only 5 results
 	numRecipes = 5;
+	//Repeat for ingredients 2 and 3 if user put them in
 	ingre2 = $("#second-ingredient").val().trim();
 	ingre3 = $("#third-ingredient").val().trim();
 
+	//If user puts in the first ingredient, it will be stored in the URL
 	if (parseInt(ingre1)) {
 		searchURL = searchURL + "&ingre1=" + ingre1 + "0101";
 	}
-
+	//If user puts in the first ingredient, the second ingredient will be stored in the URL
 	if (parseInt(ingre2)) {
 		searchURL = searchURL + "&ingre2=" + ingre2 + "0101";
 	}
-
+	//If user puts in the first ingredient and a second ingredient, the third ingredient will be stored in the URL
 	if (parseInt(ingre3)) {
 		searchURL = searchURL + "&ingre3=" + ingre3 + "0101";
 	}
-
+	//Run query with results and URLS
 	runQuery(numRecipes, searchURL);
 });
 
+// When user presses reset button, everything is cleared
 $("#add-reset-button").click(function() {
 	recipeCounter = 0;
 	$("well-section").empty();
 });
+
+$("#")
+
+//signup button
